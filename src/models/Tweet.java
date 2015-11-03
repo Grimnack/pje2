035053a3faf558a85994f33a473aab8e1b673145
Antonin Durey <dureyantonin@gmail.com>
2015-11-03 10:09:23 +0100

    Changement dans le code existant pour changer les String concernant les Tweet en classe Tweet

--- src/models/Tweet.java
+++ src/models/Tweet.java
@@ -9,13 +9,44 @@ public class Tweet {
 		this.polarite = Polarite.UNDEFINED ;
 	}
 	
+	public Tweet(String text){
+		this();
+		setText(text);
+	}
+	
+	public Tweet(String text, int polarite){
+		setText(text);
+		setPolarite(polarite);
+	}
+	
 	public void setText(String text){
 		this.text = text ;
 	}
+	
 	public void setUser(String user){
 		this.user = user ;
 	}
+	
+	public void setPolarite(int polarite){
+		switch(polarite){
+			case 0:
+				this.polarite = Polarite.NEGATIF;
+				break;
+			case 2:
+				this.polarite = Polarite.NEUTRE;
+				break;
+			case 4:
+				this.polarite = Polarite.POSITIF;
+				break;
+		}
+	}
+	
 	public void setPolarite(Polarite pol){
 		this.polarite = pol ;
 	}
+	
+	public String getText(){
+		return text;
+	}
+	
 }
